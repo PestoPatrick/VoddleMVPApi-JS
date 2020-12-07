ec38baaca0476db505741245864c50f0120eefc5
patrick <paddymcpherson@hotmail.com>
2020-12-07 23:25:10 +0000

    remembered how to handle url parameters!

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/:userid', (req,res,next) => {
+  db.one("Select * FROM voddle.Tblusers WHERE Userid = $1", req.params.userid)
+      .then(user => {
+        res.status(200).json(user)
+      })
+      .catch(error => {
+        res.error();
+      });
+})
+
 module.exports = router;
