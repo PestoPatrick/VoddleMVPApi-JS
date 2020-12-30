e4929a00807b95150ad34c461bd4700521b5ab2f
patrick <paddymcpherson@hotmail.com>
2020-12-30 19:31:06 +0000

    installed multer and multer-s3

--- helpers/VideoOperations.js
+++ helpers/VideoOperations.js
@@ -1,2 +1,16 @@
 let ffmpeg = require("fluent-ffmpeg");
 let command = ffmpeg();
+
+function ConvertVideoToMP4(video) {
+
+}
+
+function ConvertVideoToManifests(video) {
+
+}
+
+function UploadVideoToS3(video) {
+
+}
+
+module.exports = ConvertVideoToMP4, ConvertVideoToManifests, UploadVideoToS3;
